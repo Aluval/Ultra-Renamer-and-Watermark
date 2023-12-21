d233785a82892243257fe39674f6ac093acf8fee
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2023-12-21 12:20:53 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,15 +12,15 @@ logging.basicConfig(
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "10811400"))
+    API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAEpXd8rpH9lzhn7jKHp7V6VuHLFF9e3Fy4")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 5339194287))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 6469754522))
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
-    FORCE_SUB = os.environ.get("FORCE_SUB", "beta_bot_updates")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://CONVERTER24BOT:CONVERTER24BOT@cluster0.2kl0qhx.mongodb.net/?retryWrites=true&w=majority")
+    FORCE_SUB = os.environ.get("FORCE_SUB", "Sunrises24BotUpdates")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002112896731"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
