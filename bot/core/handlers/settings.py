796e5d03c624a6e9f2661384a4e3fd1e6a0b1bd7
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-21 14:44:01 +0530

    Update settings.py

--- bot/core/handlers/settings.py
+++ bot/core/handlers/settings.py
@@ -42,7 +42,7 @@ async def show_settings(m: "types.Message"):
             text="**Here you can setup your settings:**",
             reply_markup=types.InlineKeyboardMarkup(buttons_markup),
             disable_web_page_preview=True,
-            parse_mode="Markdown"
+            parse_mode="markdown"
         )
     except errors.MessageNotModified: pass
     except errors.FloodWait as e:
